dfdbec8ce5bf835d68828f2c2c0217dee6e93ec0
akaFM <orca55556@gmail.com>
2025-01-30 13:41:16 -0800

    updating models.py to include Task and User with association

--- taskFlow/tasks/models.py
+++ taskFlow/tasks/models.py
@@ -1,5 +1,25 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    """ inherits from AbstractUser, provided by Django """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) # pk - id
+
+class Task(models.Model):
+    """ task model """
+    class TaskType(models.TextChoices):
+        FUN = "Fun"
+        WORK = "Work"
+        SCHOOL = "School"
+        CHORE = "Chore"
+        OTHER = "Other"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) # pk - id
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")  # fk - user
+    description = models.TextField(default="A very important task.") 
+    type = models.CharField(max_length=20, choices=TaskType.choices, default=TaskType.OTHER) # default - OTHER
+    urgency = models.IntegerField(default=3)  # 1 (most urgent) to 5 (least urgent)
+    due_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
