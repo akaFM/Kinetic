4760d86fe54bb2e1fd58436d0acafde4532e5787
akaFM <orca55556@gmail.com>
2025-01-30 14:02:08 -0800

    create task backend barebones

--- taskFlow/tasks/urls.py
+++ taskFlow/tasks/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
      path("", views.index, name="index"),
      path("login", views.login, name="login"),
-     path("logout", views.logout, name="logout")
+     path("logout", views.logout, name="logout"),
+     path("create-task", views.create_task, name="create_task")
  ]
