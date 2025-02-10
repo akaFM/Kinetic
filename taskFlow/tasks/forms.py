f1dd25bbcf4c59e0da66b3c4a8dc828333d9d7cd
Amaan Tootla <amaantootla@gmail.com>
2025-02-10 14:53:15 -0800

    login page styled

--- taskFlow/tasks/forms.py
+++ taskFlow/tasks/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import Task
 
 class regsiterLogin(forms.Form):
-    username = forms.CharField(label="username", max_length=100)
-    password = forms.CharField(label="password", widget=forms.PasswordInput(), max_length=100)
+    username = forms.CharField(label="username", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your username'}))
+    password = forms.CharField(label="password", widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your password'}), max_length=100)
 
 
 class TaskForm(forms.ModelForm): # used to create a new task
