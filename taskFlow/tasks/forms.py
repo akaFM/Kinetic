8701336ae99641d85e39a9e4a2e44ac5cd0c5b9c
Amaan Tootla <amaantootla@gmail.com>
2025-02-10 18:18:38 -0800

    basics of dashboard UI, added helper custom template filter

--- taskFlow/tasks/forms.py
+++ taskFlow/tasks/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Task
+from calendar import month_name
 
 class regsiterLogin(forms.Form):
     username = forms.CharField(label="username", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your username'}))
@@ -14,9 +15,10 @@ class TaskForm(forms.ModelForm): # used to create a new task
             'due_date': forms.DateInput(attrs={'type': 'date'})
         }
 
+
 class calendarChoice(forms.Form):
-    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=False)
+    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}), required=False)
 
 
 class categoryChoice(forms.Form):
-    category = forms.ChoiceField(choices=[('', 'Select')] + list(Task.TaskType.choices), widget=forms.Select(), required=False)
+    category = forms.ChoiceField(choices=[('', 'All Tasks')] + list(Task.TaskType.choices), widget=forms.Select(attrs={'class': 'form-select'}), required=False)
