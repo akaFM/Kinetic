e598b3bd79583c2199819293baaaa6c896426fe8
carloscza <carrloscarranza@gmail.com>
2025-02-26 15:22:11 -0800

    Get day tasks from backend for calendar day view pop up.

--- taskFlow/tasks/urls.py
+++ taskFlow/tasks/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
      path("", views.index, name="index"),
      path("login", views.login, name="login"),
      path("logout", views.logout, name="logout"),
-     path("create-task", views.create_task, name="create_task")
+     path("create-task", views.create_task, name="create_task"),
+     path('tasks/<int:year>/<int:month>/<int:day>/', views.get_day_tasks_description_json, name='get_day_tasks_description_json')
  ]
